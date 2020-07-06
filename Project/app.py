a4a9fc5e6146c25f04684627c9443eaa963a85d4
CHIRU-17BCI0152 <44204191+CHIRU3223@users.noreply.github.com>
2020-07-06 06:23:27 +0000

    Add files via upload

---
+++ Project/app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.metrics import precision_score, recall_score 
+
+def main():
+    st.title("Binary Classification Web App")
+    st.sidebar.title("Binary Classification Web App")
+    st.markdown("Are mushrooms edible or poisonous? 🍄")
+    st.sidebar.markdown("Are mushrooms edible or poisonous? 🍄")
+
+    @st.cache(persist=True)
+    def load_data():
+        data=pd.read_csv('/home/rhyme/Desktop/Project/mushrooms.csv')
+        label=LabelEncoder()
+        for col in data.columns:
+            data[col]=label.fit_transform(data[col])
+        return data
+    
+    df=load_data()  
+
+    if st.checkbox("Show raw data",False):
+        st.subheader("Mushroom Data Set(Classification)")
+        st.write(df)
+
+if __name__ == '__main__':
+    main()
+
+
